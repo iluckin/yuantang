68e80ad1ed7c03acb9ba2c23080483eed10d4bd8
 <Apple@edzdeMacBook-Pro-5.local>
2019-07-30 14:17:24 +0800

    update

---
+++ dist/precache-manifest.4ec1b7a23bd8e7aa559be1c8302b23a6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "50f7977e7bffa5940ed6",
+    "url": "/public/static/js/about.94811db5.js"
+  },
+  {
+    "revision": "400905484deb302541f8",
+    "url": "/public/static/css/app.f1d59029.css"
+  },
+  {
+    "revision": "400905484deb302541f8",
+    "url": "/public/static/js/app.264caa5f.js"
+  },
+  {
+    "revision": "99d1dda9d68dc641d6c4",
+    "url": "/public/static/css/chunk-51f3f07e.d35dce05.css"
+  },
+  {
+    "revision": "99d1dda9d68dc641d6c4",
+    "url": "/public/static/js/chunk-51f3f07e.9d6b1d8e.js"
+  },
+  {
+    "revision": "e438309ba7f52f3a74f8",
+    "url": "/public/static/css/chunk-vendors.0465f18d.css"
+  },
+  {
+    "revision": "e438309ba7f52f3a74f8",
+    "url": "/public/static/js/chunk-vendors.ce8ea604.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/public/static/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "c5229109ca8c3f35992a25c1846de631",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
